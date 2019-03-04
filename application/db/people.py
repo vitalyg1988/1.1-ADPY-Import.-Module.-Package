64ea3d72df51fe34c2028132c5f3bbfb578ba9b2
Vitaly Golubev <vitalyg1988@gmail.com>
2019-03-04 17:06:38 +0400

    commit

---
+++ application/db/people.py
@@ -0,0 +1,6 @@
+def get_employees():
+    print('get_employees')
+
+
+if __name__ == '__main__':
+    get_employees()
---
+++ application/salary.py
@@ -0,0 +1,6 @@
+def calculate_salary():
+    print('calculate_salary')
+
+
+if __name__ == '__main__':
+    calculate_salary()
